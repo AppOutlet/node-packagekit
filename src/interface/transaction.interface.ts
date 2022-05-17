66e04b19b4a106b43f3f8d3b33e1089ad32f2f4a
messiaslima <messiaslima.03@gmail.com>
2022-05-17 09:41:44 +0100

    chore: tested packages signal

--- src/interface/transaction.interface.ts
+++ src/interface/transaction.interface.ts
@@ -2,4 +2,17 @@ import { ClientInterface } from 'dbus-next';
 
 export interface Transaction extends ClientInterface {
     Close(): Promise<void>;
+
+    SearchNames(filter: number, values: string[]): Promise<void>;
 }
+
+export enum TransactionSignal {
+    PACKAGE = 'Package',
+}
+
+export enum PackageInfo {
+    INSTALLED = 1,
+    AVAILABLE = 2,
+}
+
+export enum SearchNamesFilter {}
